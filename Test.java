48f130bb0dce938434e72eba9f6c7a9fb2a2d22a
unknown <bhagwatijeetu@gmail.com>
2021-01-23 20:36:04 -0600

    modified files and add two methods

--- Test.java
+++ Test.java
@@ -3,5 +3,11 @@ public class Test{
  public Test(){
  System.out.println("Test::Class Constructor");
  }
-
+//get User
+ public void getUser(){
+	String name="GIT:";
+ }
+ public int getID(){
+	return 10;
+}
 }
